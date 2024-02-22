28690441f4aecd8a36648307dce38fdef4fbc32e
yueyuanzhi <yuanzhi.yue@inrevo.io>
2024-02-22 17:28:37 +0800

    init

---
+++ src/main/java/org/oizehsgl/javaLearning/java/JavaLearningJavaApplication.java
@@ -0,0 +1,13 @@
+package org.oizehsgl.javaLearning.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningJavaApplication.class, args);
+	}
+
+}
